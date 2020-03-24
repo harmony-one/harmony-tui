cbc2a36d20a1632709a3f097644cdc0bf5793bab
Janet Liang <janet@harmony.one>
2020-03-24 16:56:50 -0700

    [project] Source setup env at the start of the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ GOARCH=amd64
 BINARY_DIR=bin
 BINARY=${BINARY_DIR}/harmony-tui
 
+source $(go env GOPATH)/src/github.com/harmony-one/harmony/scripts/setup_bls_build_flags.sh
+
 if [ "$(uname -s)" == "Darwin" ]; then
    GOOS=darwin
 fi
@@ -23,7 +25,7 @@ export GO111MODULE=on
 export ldflags="-X main.version=v${version} -X main.commit=${commit} -X main.builtAt=${built_at} -X main.builtBy=${built_by}"
 
 if [ "$STATIC" == "true" ]; then
-   source $(go env GOPATH)/src/github.com/harmony-one/harmony/scripts/setup_bls_build_flags.sh && env GOOS=${GOOS} GOARCH=amd64 go build -v -ldflags="${ldflags} -linkmode external -extldflags -static" -o $BINARY
+   env GOOS=${GOOS} GOARCH=amd64 go build -v -ldflags="${ldflags} -linkmode external -extldflags -static" -o $BINARY
 else
-   source $(go env GOPATH)/src/github.com/harmony-one/harmony/scripts/setup_bls_build_flags.sh && env GOOS=${GOOS} GOARCH=amd64 go build -v -ldflags="${ldflags}" -o $BINARY
+   env GOOS=${GOOS} GOARCH=amd64 go build -v -ldflags="${ldflags}" -o $BINARY
 fi
